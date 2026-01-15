0a5bf4966fdd1c80850e48302e2ca8334ed8a2a7
Shivam-Gupta-git <shivamgupta73014@gmail.com>
2026-01-15 12:19:56 +0530

    Contact Model

--- CRUD_Application/backend/model/contact.model.js
+++ CRUD_Application/backend/model/contact.model.js
@@ -3,12 +3,29 @@ import mongoose from "mongoose";
 const contactSchema = new mongoose.Schema({
   name: {
     type: String,
-    require: [true, 'Name should be required']
+    require: [true, 'Name should be required'],
+    trim: true
+  },
+  email: {
+    type: String,
+    require: [true, 'email should be required'],
+    unique: true,
+    trim: true
   },
   number:{
     type: String,
     require: [true, 'Number should be required'],
-    unique: true
+    unique: true,
+    trim: true
+  },
+  status:{
+   type: String,
+   default: 'Actice',
+   enum: ['Active', 'Inactive']
+  },
+  createdAt: {
+    type: Date,
+    default: Date.now
   }
 },{ timestamps: true })
 
